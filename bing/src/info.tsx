7f97ffbc8525a81c6705cd9bb35e6d914c0b1a8f
addis-semagn <asemagn@gmail.com>
2019-06-02 09:38:28 -0400

    changes

--- bing/src/info.tsx
+++ bing/src/info.tsx
@@ -5,7 +5,7 @@ import { RouterProps } from 'react-router';
 export const DappInfo: React.SFC<RouterProps> = (props) => {
 
   const contactAddress: string = 'b7b24896288e6f1f99f9ae49f54cfc170522f8ae';
-// const publicKey: string = '02d8c6864b40cafe07157e7f741fb6107003ad8025a6eb6ba67517d54a8baddc13';
+  const publicKey: string = '02d8c6864b40cafe07157e7f741fb6107003ad8025a6eb6ba67517d54a8baddc13';
 
   async function ScVerify(values: any) {
     const scriptHash: string = contactAddress; // contract address
@@ -24,6 +24,33 @@ export const DappInfo: React.SFC<RouterProps> = (props) => {
         gasPrice,
         gasLimit,
         requireIdentity
+      });
+      // tslint:disable-next-line:no-console
+      console.log('onScCall finished, result:' + JSON.stringify(result));
+    } catch (e) {
+      alert('onScCall canceled');
+      // tslint:disable-next-line:no-console
+      console.log('onScCall error:', e);
+    }
+  }
+
+  async function ScAdd(values: any) {
+    const scriptHash: string = contactAddress; // contract address
+    const operation: string = 'CertifyPerson'; // function name
+    const gasPrice: number = Number(500); // gas price
+    const gasLimit: number = Number(100000); // gas limit
+    const parametersRaw: any[] = [
+        { type: 'ByteArray', value: publicKey },
+        { type: 'String', value: 'cpr' }]; // function paramers
+
+    const args = parametersRaw.map((raw) => ({ type: raw.type, value: convertValue(raw.value, raw.type) }));
+    try {
+      const result = await client.api.smartContract.invoke({
+        scriptHash,
+        operation,
+        args,
+        gasPrice,
+        gasLimit
       });
       // tslint:disable-next-line:no-console
       console.log('onScCall finished, result:' + JSON.stringify(result));
@@ -104,7 +131,7 @@ export const DappInfo: React.SFC<RouterProps> = (props) => {
             </div>
 
             {/* <div className="add-button"> */}
-                <h2 className="">+ Add credentials</h2>
+                <h2 className="" onClick={ScAdd}>+ Add credentials</h2>
             {/* </div> */}
       </div>
     </div>
